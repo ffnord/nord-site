409f2628d5e0af0fd6d198779073eb9d217dc84c
Ruben Barkow <github@r.z11.de>
2019-10-07 21:13:34 +0200

    nur Target ar71xx-generic beuen (z.b. tiny target gab es noch nicht)

--- .gitlab-ci/build.sh
+++ .gitlab-ci/build.sh
@@ -165,20 +165,21 @@ fi
 if [[ -z ${TARGETS+x} ]] ; then
   case "${BRANCH}" in
     stable)
-      TARGETS="ar71xx-tiny ar71xx-generic"
-      TARGETS+=" x86-64 x86-generic" # (VMs)
-      TARGETS+=" ar71xx-nand" # (Netgear WNDR3700, WNDR4300, ZyXEL NBG6716)
-      TARGETS+=" mpc85xx-generic" # (tp-link-tl-wdr4900-v1)
-      TARGETS+=" ramips-mt7620" # (gl-inet mt300 und mt750)
+      TARGETS="ar71xx-generic"
+      #TARGETS="ar71xx-tiny" # since 2017
+      #TARGETS+=" x86-64 x86-generic" # (VMs)
+      #TARGETS+=" ar71xx-nand" # (Netgear WNDR3700, WNDR4300, ZyXEL NBG6716)
+      #TARGETS+=" mpc85xx-generic" # (tp-link-tl-wdr4900-v1)
+      #TARGETS+=" ramips-mt7620" # (gl-inet mt300 und mt750)
       #TARGETS+=" sunxi-cortexa7" # (Banana Pi M1)
 
       # BROKEN:
       #TARGETS+=" brcm2708-bcm2708 brcm2708-bcm2709 brcm2708-bcm2710" # (raspberry Pi 1, 2 und 3)
-      TARGETS+=" ipq40xx" # (FitzBox 4040)
+      #TARGETS+=" ipq40xx" # (FitzBox 4040)
       #TARGETS+=" ramips-mt7621" # (D-Link DIR-860L (B1) Ubiquiti EdgeRouter X, ZBT WG3526)
-      TARGETS+=" x86-geode"
+      #TARGETS+=" x86-geode"
       #TARGETS+=" ramips-rt305x" # BROKEN: (fonera, vocore a5)
-      TARGETS+=" ramips-mt76x8" # BROKEN: unstable WiFi (tp-link 841 v13 und archer c50)
+      #TARGETS+=" ramips-mt76x8" # BROKEN: unstable WiFi (tp-link 841 v13 und archer c50)
       #TARGETS+=" ar71xx-mikrotik" # BROKEN: no sysupgrade support (mikrotik-nand)
       #TARGETS+=" brcm2708-bcm2710" # BROKEN: Untested (raspberry-pi-3)
       #TARGETS+=" ipq806x" # BROKEN: unstable wifi drivers (tp-link-archer-c2600)
@@ -186,8 +187,7 @@ if [[ -z ${TARGETS+x} ]] ; then
       ;;
     *)
       # Default to all targets
-      TARGETS="ar71xx-generic ar71xx-tiny ar71xx-nand brcm2708-bcm2708 brcm2708-bcm2709 ramips-mt7621 x86-generic x86-geode x86-64 ramips-mt7620 ramips-mt76x8 ramips-rt305x sunxi-cortexa7 ipq40xx"
-      TARGETS+=" mpc85xx-generic" # (tp-link-tl-wdr4900-v1)-
+      TARGETS="ar71xx-generic" # (tp-link-tl-wdr4900-v1)-
     ;;
   esac
 fi
